5d28d83ebe2c4a3fad646ac8400dae1fe126e86a
Nature-Studioz <69017069+Nature-Studioz@users.noreply.github.com>
2021-11-05 15:45:29 -0400

    Some Summary

---
+++ MergeGame/Assets/Merge.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Merge : MonoBehaviour
+{
+    int ID;
+    public GameObject MergedObject;
+    Transform Block1;
+    Transform Block2;
+    public float Distance;
+    public float MergeSpeed;
+    bool CanMerge;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ID = GetInstanceID();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        MoveTowards();
+    }
+    public void MoveTowards()
+    {
+        if (CanMerge)
+        {
+            transform.position = Vector2.MoveTowards(Block1.position, Block2.position, MergeSpeed);
+            if (Vector2.Distance(Block1.position, Block2.position) < Distance)
+            {
+                if (ID < Block2.gameObject.GetComponent<Merge>().ID) { return; }
+                Debug.Log($"SENDING MESSAGE FROM {gameObject.name} With The ID Number of{ID}");
+                GameObject O = Instantiate(MergedObject, transform.position, Quaternion.identity) as GameObject;
+                Destroy(Block2.gameObject);
+                Destroy(gameObject);
+            }
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("MergeBlock"))
+        {
+            if(collision.gameObject.GetComponent<SpriteRenderer>().color == GetComponent<SpriteRenderer>().color)
+            {
+                Block1 = transform;
+                Block2 = collision.transform;
+                CanMerge = true;
+                Destroy(collision.gameObject.GetComponent<Rigidbody2D>());
+                Destroy(GetComponent<Rigidbody2D>());
+            }
+        }
+    }
+}
